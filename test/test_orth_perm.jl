72887977e9d56e91bdd579bfe91ff51d2a7b3590
Genevieve <g.dusson@warwick.ac.uk>
2019-09-30 10:51:40 +0200

    set up the test.

--- test/test_orth_perm.jl
+++ test/test_orth_perm.jl
@@ -12,23 +12,6 @@ using Combinatorics
 ##
 @info("Testing orthogonality of permutation-invariant Ylm via sampling")
 
-function scalar_prod(L1,M1,L2,M2,SH,N,Nsamples = 1_000_000)
-   res = 0.
-   I1 = [SHIPs.SphericalHarmonics.index_y(L1[i],M1[i]) for i in 1:N]
-   I2 = [SHIPs.SphericalHarmonics.index_y(L2[i],M2[i]) for i in 1:N]
-   for n=1:Nsamples
-      # generate samples
-      Rs = [SHIPs.Utils.rand_sphere() for i=1:N]
-      Y = [SHIPs.eval_basis(SH, Rs[i]) for i=1:N]
-
-      # Sum over permutations
-      BL1M1 = sum(prod(Y[σ[i]][I1[i]] for i in 1:N) for σ in permutations(1:N))
-      BL2M2 = sum(prod(Y[σ[i]][I2[i]] for i in 1:N) for σ in permutations(1:N))
-      res += BL1M1*BL2M2
-   end
-   return res / Nsamples
-end
-
 
 function scalar_prod(LM1,LM2,SH,N,Nsamples = 1_000_000)
    res = 0.
@@ -48,17 +31,6 @@ function scalar_prod(LM1,LM2,SH,N,Nsamples = 1_000_000)
 end
 
 
-
-function rand_test(maxL,N,Nsamples = 1_000_000)
-   SH = SHIPs.SphericalHarmonics.SHBasis(maxL)
-   L1 =  rand(collect(0:maxL),N)
-   L2 =  rand(collect(0:maxL),N)
-   M1 = [rand(-L1[i]:L1[i]) for i in 1:N]
-   M2 = [rand(-L2[i]:L2[i]) for i in 1:N]
-   r = scalar_prod(L1,M1,L2,M2,SH,N,Nsamples)
-   @show L1,L2,M1,M2,r
-end
-
 function rand_test(maxL,N,Nsamples = 1_000_000)
    SH = SHIPs.SphericalHarmonics.SHBasis(maxL)
    LM1 = Tuple{Int64,Int64}[]
@@ -72,12 +44,45 @@ function rand_test(maxL,N,Nsamples = 1_000_000)
       push!(LM1,(l1,m1))
       push!(LM2,(l2,m2))
    end
-   r = scalar_prod(LM1,LM2,SH,N,Nsamples)
-   @show LM1,LM2,r
+   if sort(LM1) != sort(LM2)
+      return scalar_prod(LM1,LM2,SH,N,Nsamples)
+   else
+      return 0.
+   end
 end
 
-rand_test(4,2)
 
+@test abs.(rand_test(4,2))<1e-4
+
+222
+
+# function rand_test(maxL,N,Nsamples = 1_000_000)
+#    SH = SHIPs.SphericalHarmonics.SHBasis(maxL)
+#    L1 =  rand(collect(0:maxL),N)
+#    L2 =  rand(collect(0:maxL),N)
+#    M1 = [rand(-L1[i]:L1[i]) for i in 1:N]
+#    M2 = [rand(-L2[i]:L2[i]) for i in 1:N]
+#    r = scalar_prod(L1,M1,L2,M2,SH,N,Nsamples)
+#    @show L1,L2,M1,M2,r
+# end
+
+
+# function scalar_prod(L1,M1,L2,M2,SH,N,Nsamples = 1_000_000)
+#    res = 0.
+#    I1 = [SHIPs.SphericalHarmonics.index_y(L1[i],M1[i]) for i in 1:N]
+#    I2 = [SHIPs.SphericalHarmonics.index_y(L2[i],M2[i]) for i in 1:N]
+#    for n=1:Nsamples
+#       # generate samples
+#       Rs = [SHIPs.Utils.rand_sphere() for i=1:N]
+#       Y = [SHIPs.eval_basis(SH, Rs[i]) for i=1:N]
+#
+#       # Sum over permutations
+#       BL1M1 = sum(prod(Y[σ[i]][I1[i]] for i in 1:N) for σ in permutations(1:N))
+#       BL2M2 = sum(prod(Y[σ[i]][I2[i]] for i in 1:N) for σ in permutations(1:N))
+#       res += BL1M1*BL2M2
+#    end
+#    return res / Nsamples
+# end
 
 
 # function gram_matrix(Llist,Mlist,Nsamples = 10)
